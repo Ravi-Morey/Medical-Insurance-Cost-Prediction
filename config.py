9380e0eb6d4ee46d0c5e4dd20708257139c4e6cb
Ravi-Morey <ravimorey9545@gmail.com>
2023-02-04 00:46:49 +0530

    Project Configuration

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+CSV_FILE_PATH = "enter_path"
+
+PORT_NUMBER = 7000
+
+MODEL_FILE_PATH = os.path.join("models", "medical_model.pkl")
+
+JSON_FILE_PATH = os.path.join("models", "medical_data.json")
